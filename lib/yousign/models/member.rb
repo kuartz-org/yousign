11afacf8241ea5b9ccc093858ce6fc0ccc680398
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2022-06-06 11:51:43 +0200

    Update naming

--- lib/yousign/models/member.rb
+++ lib/yousign/models/member.rb
@@ -27,7 +27,9 @@ module Yousign
                 :type,
                 :user
 
-    def signable_document_url
+    ##
+    # URL for the member to sign a procedure
+    def signable_procedure_url
       URI("#{Yousign.config.webapp_url}/procedure/sign?members=#{id}").to_s
     end
   end
